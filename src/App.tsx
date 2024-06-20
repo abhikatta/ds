76189b2c361c7734ddfa0a68d959a5f7fed8417e
abhikatta <abhinaykatta97@gmail.com>
2024-06-20 20:19:41 +0530

    fixed appendAfterValue function

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,28 @@ import { LinkedList } from "./LinkedList";
 
 const App = () => {
   const ll = new LinkedList(12);
-  ll.append(13);
-  ll.append(14);
-  ll.appendAfterValue(14, 18);
-  ll.appendAtindex(2, 10);
-  ll.prepend(9);
 
-  console.log(ll.traverse());
+  console.log(ll.traverse(), ll.length);
+
+  ll.append(14);
+  console.log(ll.traverse(), ll.length);
+
+  ll.appendAfterValue(14, 18);
+  console.log(ll.traverse(), ll.length);
+
+  ll.appendAtindex(2, 10);
+  console.log(ll.traverse(), ll.length);
+
+  ll.prepend(9);
+  console.log(ll.traverse(), ll.length);
+
   ll.popElement(12);
+  console.log(ll.traverse(), ll.length);
+
   ll.popElement(9);
-  console.log(ll.traverse());
+  console.log(ll.traverse(), ll.length);
   ll.shift();
+  console.log(ll.traverse(), ll.length);
 
   return <div>App</div>;
 };
--- src/LinkedList.ts
+++ src/LinkedList.ts
@@ -9,8 +9,10 @@ export class NodeElement {
 }
 export class LinkedList {
   private head: NodeElement | null;
+  length: number = 0;
   constructor(value: val) {
     this.head = new NodeElement(value);
+    this.length += 1;
   }
   /**Appends an element at the end of the list, creates new list with element if list does not exist. */
   append = (value: val): void => {
@@ -22,38 +24,50 @@ export class LinkedList {
       current = current.next;
     }
     current.next = new NodeElement(value);
+    this.length += 1;
   };
   /**Appends an element at the begining of the list */
   prepend = (value: val): void => {
     if (!this.head) {
       this.head = new NodeElement(value);
+      this.length += 1;
     }
     const current = this.head;
     const newHead = new NodeElement(value);
     this.head = newHead;
     newHead.next = current;
+    this.length += 1;
   };
 
-  /** Appends value to the list after specific value if the value exists, else throws error if list is empty*/
-  appendAfterValue = (position: val, value: val): void => {
+  /** Appends value to the list after specific value if the value exists, else throws error if list is empty or returns null if element is not found.*/
+  appendAfterValue = (position: val, value: val): void | null => {
     if (!this.head) {
       throw Error("List is null!");
     }
     let current = this.head;
-    while (current?.next) {
+    while (current) {
       if (current?.val === position) {
         const element = new NodeElement(value);
         const currentNext = current.next;
         current.next = element;
         element.next = currentNext;
+        this.length += 1;
+        break;
+      } else if (current.next === null) {
         break;
       }
       current = current.next;
     }
+    return null;
   };
 
   /** Appends value to the list, else throws error if list is empty*/
   appendAtindex = (index: number, value: val): void => {
+    if (index > this.length || index < 0) {
+      throw Error(
+        `Index is invalid! List length is ${this.length}, given index is ${index}`
+      );
+    }
     if (!this.head) {
       throw Error("List is null!");
     }
@@ -66,6 +80,7 @@ export class LinkedList {
           const element = new NodeElement(value);
           current.next = element;
           element.next = currentNext;
+          this.length += 1;
           break;
         }
         current = current.next;
@@ -100,6 +115,7 @@ export class LinkedList {
     } else if (this.head && !this.head.next) {
       const element = this.head;
       this.head = null;
+      this.length -= 1;
       return element;
     } else {
       let current = this.head;
@@ -107,6 +123,7 @@ export class LinkedList {
         if (!current.next.next) {
           const element = current.next;
           current.next = null;
+          this.length -= 1;
           return element;
         }
         current = current.next;
@@ -125,6 +142,7 @@ export class LinkedList {
     while (current.next) {
       if (count + 1 === index) {
         current.next = current.next.next;
+        this.length -= 1;
         break;
       }
       count += 1;
@@ -137,16 +155,19 @@ export class LinkedList {
   popElement = (value: val): NodeElement | null => {
     if (!this.head) {
       return null;
-    }
-    if (this.head.val === value) {
+    } else if (this.head.val === value) {
       const element = this.head;
       this.head = this.head.next;
+      this.length -= 1;
       return element;
     } else {
       let current = this.head;
       while (current.next) {
         if (current.next.val === value) {
-          const element = current;
+          const element = current.next;
+          current.next = element.next;
+          element.next = null;
+          this.length -= 1;
           return element;
         }
         current = current.next;
@@ -161,6 +182,7 @@ export class LinkedList {
     } else {
       const element = this.head;
       this.head = this.head.next;
+      this.length -= 1;
       return element;
     }
   };
