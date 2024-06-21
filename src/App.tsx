ea92719bf417540cf1214b8a02b09944ef134a36
abhikatta <abhinaykatta97@gmail.com>
2024-06-21 14:58:02 +0530

    fixed pop and shift functionality, now has proper UI updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,61 @@
-import React, { useEffect, useMemo, useState } from "react";
+import { useState } from "react";
 import { LinkedList } from "./LinkedList";
 import { val } from "./types/node";
 
 const App = () => {
-  const [elements, setElements] = useState<val[]>([]);
+  const [inputString, setInputString] = useState("");
   const [values, setValues] = useState<val[] | null>([]);
-  const ll = new LinkedList(0);
-  ll.pop();
-  const handleChange = (vals: string) => {
-    const elements = vals.split(" ");
-    setElements(elements);
-  };
+  const [submitClicked, setSubmitClicked] = useState(false);
+  const [list, setList] = useState<LinkedList>();
+  const handleShow = () => {
+    setSubmitClicked(true);
+    const inputStringArray = inputString.trim().split(" ");
+    const ll = new LinkedList(inputStringArray[0]);
 
-  const showElements = () => {
-    if (elements) {
-      elements.forEach((element) => {
-        ll.append(element);
-      });
-      setValues(ll.traverse());
+    inputStringArray.map((item, index) => {
+      index > 0 && ll.append(item);
+    });
+    setValues(ll.traverse());
+    setList(ll);
+  };
+  const Pop = () => {
+    if (list) {
+      list.pop();
+      setValues(list.traverse());
+    }
+  };
+  const Shift = () => {
+    if (list) {
+      list.shift();
+      setValues(list.traverse());
     }
   };
 
+  const clearAll = () => {
+    setSubmitClicked(false);
+    setInputString("");
+    setValues([]);
+  };
   return (
     <div>
       <input
-        onChange={(e) => handleChange(e.target.value)}
+        disabled={submitClicked}
+        value={inputString}
+        onChange={(e) => setInputString(e.target.value)}
         placeholder="Enter array elements with elements separated by spaces"
       />
-      <button onClick={showElements}>Show</button>
       <button
+        disabled={submitClicked}
         onClick={() => {
-          ll.pop();
-          showElements();
+          inputString.trim().length > 0 && handleShow();
         }}>
-        Pop
+        Show
       </button>
-      <button onClick={() => ll.shift()}>Shift</button>
+      <button disabled={!submitClicked} onClick={clearAll}>
+        Clear Input
+      </button>
+      <button onClick={Pop}>Pop</button>
+      <button onClick={Shift}>Shift</button>
       {/* <div><button>Pop At Index</button></div> */}
 
       <div style={{ flexDirection: "row", display: "flex" }}>
