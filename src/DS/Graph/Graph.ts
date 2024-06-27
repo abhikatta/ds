f025f7ded540c7d826b8c9a773b612d30093c151
abhikatta <abhinaykatta97@gmail.com>
2024-06-27 11:26:00 +0530

    implemented graph using adjacency matrix and adjacency list

---
+++ src/DS/Graph/Graph.ts
@@ -0,0 +1,44 @@
+// export class Graph {
+//   matrix: number[][] = [[]];
+//   constructor(numNodes: number) {
+//     this.matrix = [];
+//     for (let i = 0; i < numNodes; i++) {
+//       this.matrix.push(new Array(numNodes).fill(0));
+//     }
+//   }
+//   addEdge(fromNode: number, toNode: number) {
+//     this.matrix[fromNode][toNode] = 1;
+//     this.matrix[toNode][fromNode] = 1;
+//   }
+//   removeEdge(fromNode: number, toNode: number) {
+//     this.matrix[fromNode][toNode] = 0;
+//     this.matrix[toNode][fromNode] = 0;
+//   }
+//   isEdge(fromNode: number, toNode: number) {
+//     return this.matrix[fromNode][toNode] === 1;
+//   }
+// }
+
+type listType = {
+  [key: number]: number[];
+};
+export class Graph {
+  list: listType = {};
+  constructor() {
+    this.list = {};
+  }
+  addNode(node: number) {
+    this.list[node] = [];
+  }
+  addEdge(fromNode: number, toNode: number) {
+    this.list[fromNode].push(toNode);
+    this.list[toNode].push(fromNode);
+  }
+  removeEdge(fromNode: number, toNode: number) {
+    this.list[fromNode] = this.list[fromNode].filter((node) => node !== toNode);
+    this.list[toNode] = this.list[toNode].filter((node) => node !== fromNode);
+  }
+  isEdge(fromNode: number, toNode: number) {
+    return this.list[fromNode].includes(toNode);
+  }
+}
